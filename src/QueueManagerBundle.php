eb803c8229c032288da16ce3e2af068408a062ad
Markom <mariukaz@gmail.com>
2023-01-20 05:17:41 +0200

    Updates

--- src/QueueManagerBundle.php
+++ src/QueueManagerBundle.php
@@ -3,9 +3,14 @@ declare(strict_types=1);
 
 namespace Markom\QueueManagerBundle;
 
+use Markom\QueueManagerBundle\DependencyInjection\QueueManagerExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class QueueManagerBundle extends AbstractBundle
 {
-
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new QueueManagerExtension();
+    }
 }
